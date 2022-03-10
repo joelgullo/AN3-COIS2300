26565a5c0bd9240871053ff53e68b24601f92af6
matthewberes <60519715+matthewberes@users.noreply.github.com>
2022-03-09 23:19:01 -0500

    Update main.cs
    Main method calling both loop functions from Queue.cs, getting hung up on first function because it always returns null

--- FCFS/FCFS/main.cs
+++ FCFS/FCFS/main.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 
-public static class MainProgram
+public class MainProgram
 {
     //**********************************************************************
     // Name: Main
@@ -15,8 +15,7 @@ public static class MainProgram
     //    5 - frees event node after it has been processed.
     //    6 - prints out the statistics when the simulation is finished.
     //*********************************************************************
-        int[] pageArray = new int[100]; // keep track of total # of page requests
-	public static void Main()
+    public static void Main()
     {
         bool not_done;
         Cust new_index;
@@ -36,28 +35,33 @@ public static class MainProgram
         Utility.Gen_arrival(new_index, arrive_time);
         // main loop to process the event list
         not_done = true;
-        while(not_done)
-	    {
-	        // get next event
-	        eventid = Globals.ev_list.remove_event();
-	        // update clock
-	        Globals.clock = eventid.get_evtime();
-	        // process event type
-	        switch (eventid.get_evtype())
-		        {
-		        case Globals.ARRIVAL    : Arrive(eventid);
-								              break;
-		        case Globals.COMPLETE   : Depart(eventid);
-								              break;
-                case Globals.EOS        : Utility.Process_statistics();
-								            not_done = false;
-								            break;
-		        default                 : Console.WriteLine("***Error - invalid event type\n");
-                                            break;
-		        }
-	      }
+        while (not_done)
+        {
+            // get next event
+            eventid = Globals.ev_list.remove_event();
+            // update clock
+            Globals.clock = eventid.get_evtime();
+            // process event type
+            switch (eventid.get_evtype())
+            {
+                case Globals.ARRIVAL:
+                    Arrive(eventid);
+                    break;
+                case Globals.COMPLETE:
+                    Depart(eventid);
+                    break;
+                case Globals.EOS:
+                    Utility.Process_statistics();
+                    not_done = false;
+                    break;
+                default:
+                    Console.WriteLine("***Error - invalid event type\n");
+                    break;
+            }
+        }
     }
-
+    public static int page = 0;
+    static int[] pageArray = new int[101];                 // keep track of total # of page requests
 
     //*********************************************************************
     // Name: arrive                                                        
@@ -82,15 +86,15 @@ public static class MainProgram
         // set statistics gathering variable
         cur_index = ev_num.get_cust();
         cur_index.setarrive(Globals.clock);
-	    
+
         page = cur_index.getPage();
         pageArray[page]++;
-	    
+
         // put the customer in the queue
         Globals.fcfs.add_to_queue(cur_index);
         // if server is not busy then start service
-        if(!Globals.busy)
-	        StartService();
+        if (!Globals.busy)
+            StartService();
         return;
     }
 
@@ -107,15 +111,42 @@ public static class MainProgram
     public static void StartService()
     {
         long servicetime;
-        Cust index;
+        ///Cust index;
+        int p = 0;
+        //int largestTotal = 0;
+        //int curPage;
+        Cust curIndex;
+        Cust longestCust;
+        Cust[] samePage = new Cust[20];
+
+        longestCust = Globals.fcfs.firstForEach();
+        p = longestCust.getPage();
+
+        //initiallizing object for non-static member
+        //Queue q = new Queue();
+        //call method that loops through queue to find all customers requesting the most requested page
+        samePage = Globals.fcfs.secondForEachLoop(p);
+
+
+        servicetime = Utility.Expon(Globals.service_time);
+        //For each cust that has the same page, generate a departure event at the current time
+        for (int x = 0; x < samePage.Length; x++)
+        {
+            curIndex = samePage[x];
+            if (curIndex != null)
+            {
+                Utility.Gen_departure(curIndex, Globals.clock + servicetime);
+            }
+        }
+
         // remove the first customer from the queue
-        index = Globals.fcfs.take_off_queue();
+        // ***index = Globals.fcfs.take_off_queue();
         // set server to busy
         Globals.busy = true;
         // generate a CPU bust time for the next arrival and associate it with customer
-        servicetime = Utility.Expon(Globals.service_time);
+        // ***servicetime = Utility.Expon(Globals.service_time);
         // schedule a departure event based on CPU burst time
-        Utility.Gen_departure(index, servicetime);
+        // ***Utility.Gen_departure(index, servicetime);
         //accumulate busy time to compute utilization
         Globals.busytime += servicetime;
         return;
@@ -135,28 +166,24 @@ public static class MainProgram
     //*********************************************************************
     public static void Depart(EVnode ev_num)
     {
-        Cust index;
+        Cust dep_index;
         long temp;
         // set server to idle
         Globals.busy = false;
         // accumulate response time
-        index = ev_num.get_cust();
-	    
-        page = index.getPage();
+        dep_index = ev_num.get_cust();
+
+        page = dep_index.getPage();
         pageArray[page] = 0;
-	    
-        temp = Globals.clock - index.getarrive();
-        if(Globals.DEBUG)
-            Console.WriteLine(" Response time for customer {0} is {1}", index.getnum(), temp);
+
+        temp = Globals.clock - dep_index.getarrive();
+        //if (Globals.DEBUG)
+            //Console.WriteLine(" Response time for customer {0} is {1}", index.getnum(), temp);
         Globals.accum_resp_time += temp;
         Globals.num_resp_time++;
         // if queue is non-empty, start service
-        if(!Globals.fcfs.isempty())
-	        StartService();
+        if (!Globals.fcfs.isempty())
+            StartService();
         return;
     }
 }
-
-
-
-    
