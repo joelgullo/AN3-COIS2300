d4ec6ed0651241efa3195262e28d1f533239b7b4
matthewberes <60519715+matthewberes@users.noreply.github.com>
2022-03-09 23:55:40 -0500

    Update EventList.cs
    had wrong version of eventlist.cs, was using the broken one from main, fixed now

--- FCFS/FCFS/EventList.cs
+++ FCFS/FCFS/EventList.cs
@@ -1,35 +1,33 @@
-﻿using System;
+using System;
 
 // Class to hold the elements to be stored in the Event List for the Discrete Event Simulation
 public class EVnode
 {
-    private int ev_type;	    	/* event type */
-	private long ev_time;	        /* time for event to occur */
-	private Cust cust_index;	    /* which customer is responsible for this event */
-	private int pageRequested;      // Random page number
-	private EVnode forward;	        /* forward link */
-	private EVnode backward;	    /* backward link */                            
+    private int ev_type;            /* event type */
+    private long ev_time;           /* time for event to occur */
+    private Cust cust_index;        /* which customer is responsible for this event */
+    private int pageRequested;      // Random page number
+    private EVnode forward;         /* forward link */
+    private EVnode backward;        /* backward link */
 
     // Constructors
     public EVnode()
-	{
-	    ev_time = 0;
-	    ev_type = -1;
-	    cust_index = null;
-	    pageRequested = 0;
-	    forward = null;
-	    backward = null;
-	}
+    {
+        ev_time = 0;
+        ev_type = -1;
+        cust_index = null;
+        forward = null;
+        backward = null;
+    }
 
-    public EVnode(int etype, long etime, Cust ptr, int page)
-	{
-	    ev_time = etime;
-	    ev_type = etype;
-	    cust_index = ptr;
-	    pageRequested = page;
-	    forward = null;
-	    backward = null;
-	}
+    public EVnode(int etype, long etime, Cust ptr)
+    {
+        ev_time = etime;
+        ev_type = etype;
+        cust_index = ptr;
+        forward = null;
+        backward = null;
+    }
 
     // Properties
     public EVnode Forward
@@ -56,138 +54,130 @@ public class EVnode
         }
     }
     // return the Event Type from the Event node
-    public int get_evtype() 
+    public int get_evtype()
     {
         return ev_type;
     }
 
-    public long get_evtime() 
-    { 
+    public long get_evtime()
+    {
         return ev_time;
     }
 
     // return the Customer Index from the Event node
-    public Cust get_cust() 
-    { 
+    public Cust get_cust()
+    {
         return cust_index;
     }
-	
-    public long get_evpage()
-    {
-        return pageRequested;
-    }
+
 }
-
-// Class to manipulate the Event List
-public class EVlist
-{
-    int[] pageArray = new int[100]; // keep track of total # of page requests	
-	
-    private EVnode top_event;
-    private EVnode last_event;
-
-    // Constructor
-    public EVlist()
+    // Class to manipulate the Event List
+    public class EVlist
     {
-        top_event = null;
-        last_event = null;
-    }
 
+        private EVnode top_event;
+        private EVnode last_event;
 
-    // add an element of type Cust to queue
-    public void insert_event(int etype, long etime, Cust ptr, int pageRequest)
-    {
-        bool not_found;
-        EVnode loc, pos;
-        // create a new event node with the appropriate information
-        loc = new EVnode(etype, etime, ptr, pageRequest);
-        // determine if the list is empty
-        if (top_event == null)
-        {
-            top_event = loc;
-            last_event = loc;
-            return;
-        }
-        // see if it belongs on top
-        if (top_event.get_evtime() > etime)
-        {
-            top_event.Backward = loc;
-            loc.Forward = top_event;
-            top_event = loc;
-            return;
-        }
-        // see if it belongs at the bottom
-        if (last_event.get_evtime() <= etime)
-        {
-            last_event.Forward = loc;
-            loc.Backward = last_event;
-            last_event = loc;
-            return;
-        }
-        // it belongs somewhere in the middle so find its place
-        not_found = true;
-        pos = top_event;
-        while (pos != null && not_found)
-        {
-            if (pos.get_evtime() > etime)
-                not_found = false;
-            else
-                pos = pos.Forward;
-        }
-        // check to see if we found something as we should have
-        if (not_found)
-        {
-            Console.WriteLine("***Error - problems in insert event routine***\n");
-            return;
-        }
-        // add node to appropriate place
-        loc.Forward = pos;
-        loc.Backward = pos.Backward;
-        (pos.Backward).Forward = loc;
-        pos.Backward = loc;
-	    
-	pageArray[pageRequest]++;   //keeping track of the number of requests for a certain page
-	    
-        return;
-    }
-
-    //return the top event from the event list
-    public EVnode remove_event()
-    {
-        EVnode ev_ptr;
-        // check to see if event list is empty
-        if (top_event == null)
-        {
-            Console.WriteLine("***Error - Event list underflow***\n");
-            return (null);
-        }
-        // remove top element
-        ev_ptr = top_event;
-        // see if it was the only event - special case to mark empty
-        if (top_event == last_event)
+        // Constructor
+        public EVlist()
         {
             top_event = null;
             last_event = null;
+        }
+
+
+        // add an element of type Cust to queue
+        public void insert_event(int etype, long etime, Cust ptr)
+        {
+            bool not_found;
+            EVnode loc, pos;
+            // create a new event node with the appropriate information
+            loc = new EVnode(etype, etime, ptr);
+            // determine if the list is empty
+            if (top_event == null)
+            {
+                top_event = loc;
+                last_event = loc;
+                return;
+            }
+            // see if it belongs on top
+            if (top_event.get_evtime() > etime)
+            {
+                top_event.Backward = loc;
+                loc.Forward = top_event;
+                top_event = loc;
+                return;
+            }
+            // see if it belongs at the bottom
+            if (last_event.get_evtime() <= etime)
+            {
+                last_event.Forward = loc;
+                loc.Backward = last_event;
+                last_event = loc;
+                return;
+            }
+            // it belongs somewhere in the middle so find its place
+            not_found = true;
+            pos = top_event;
+            while (pos != null && not_found)
+            {
+                if (pos.get_evtime() > etime)
+                    not_found = false;
+                else
+                    pos = pos.Forward;
+            }
+            // check to see if we found something as we should have
+            if (not_found)
+            {
+                Console.WriteLine("***Error - problems in insert event routine***\n");
+                return;
+            }
+            // add node to appropriate place
+            loc.Forward = pos;
+            loc.Backward = pos.Backward;
+            (pos.Backward).Forward = loc;
+            pos.Backward = loc;
+
+            return;
+        }
+
+        //return the top event from the event list
+        public EVnode remove_event()
+        {
+            EVnode ev_ptr;
+            // check to see if event list is empty
+            if (top_event == null)
+            {
+                Console.WriteLine("***Error - Event list underflow***\n");
+                return (null);
+            }
+            // remove top element
+            ev_ptr = top_event;
+            // see if it was the only event - special case to mark empty
+            if (top_event == last_event)
+            {
+                top_event = null;
+                last_event = null;
+                return (ev_ptr);
+            }
+            // event list has more than one element so just relink
+            top_event = top_event.Forward;
+            top_event.Backward = null;
+            ev_ptr.Forward = null;
             return (ev_ptr);
         }
-        // event list has more than one element so just relink
-        top_event = top_event.Forward;
-        top_event.Backward = null;
-        ev_ptr.Forward = null;
-        return (ev_ptr);
-    }
 
-    // print out the elements in the queue
-    public void print()
-    {
-        EVnode ptr;
-        ptr = top_event;
-        Console.WriteLine("\nEvent List contents ... ");
-        while (ptr != null)
+        // print out the elements in the queue
+        public void print()
         {
-            Console.Write("\nEvent Type: {0}", ptr.get_evtype());
-            Console.WriteLine(" ... Event Time: {0}", ptr.get_evtime());
-            ptr = ptr.Forward;
+            EVnode ptr;
+            ptr = top_event;
+            Console.WriteLine("\nEvent List contents ... ");
+            while (ptr != null)
+            {
+                Console.Write("\nEvent Type: {0}", ptr.get_evtype());
+                Console.WriteLine(" ... Event Time: {0}", ptr.get_evtime());
+                ptr = ptr.Forward;
+            }
         }
     }
-}
